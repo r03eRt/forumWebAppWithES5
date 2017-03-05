01df9311cb1fb36102d62fdd300cecde182ed53c
r03eRt <morgadoluengo@gmail.com>
2017-03-05 20:00:23 +0100

    now added onload and onerror fucntion to the promise

--- app/src/js/app.js
+++ app/src/js/app.js
@@ -10,7 +10,31 @@ let Post = {
 		return new Promise(
 			function(resolve, reject)
 			{
-				resolve('ok Posts!');
+				//set the uri of the resource
+				let uri = 'http://localhost:3000/posts';
+				//new httprequest
+				let request = new XMLHttpRequest();
+				//sincronoues call 'true'
+				request.open('GET', uri, true);
+				
+				//set the onload(request loaded full maybe witherrors) callback
+				request.onload = function()
+				{
+					//if request is ok resolve this promise and pass the response to the cb on this case ui.renderPost
+					if(request.status >= 200 && request.status < 400)
+					{
+						resolve(JSON.parse(request.response));
+					}
+				}
+
+				//set the onerrir(request go wrong) and pass the response to the ui.renderPost
+				request.onerror = function()
+				{
+					reject(new Error ('Somethng wnet wrong with the API'));
+				}
+
+				//init the request
+				request.send();
 			}
 		)
 	}
@@ -18,11 +42,20 @@ let Post = {
 
 
 let ui = {
+	//is the callback of promise, when ok receive the data from resolved
 	renderPosts(posts)
 	{
-		console.log('posts');
+		console.log(posts);
 	}
 }
 
 // Promise for getting all the post well need
-Post.findAll().then(ui.renderPosts);
+Post.findAll()
+	.then(ui.renderPosts)
+	.catch( function(error)
+	{
+		console.log(error)
+	});
+
+
+
